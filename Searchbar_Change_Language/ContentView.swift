2aa02a23ab9dbba860b49419d67d1e41b333378e
Sebastian <s.kloesel@gmx.de>
2022-03-05 06:59:13 +0100

    Initial Commit

---
+++ Searchbar_Change_Language/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Searchbar_Change_Language
+//
+//  Created by Sebastian Klösel on 05.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Searchbar_Change_Language/Searchbar_Change_LanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Searchbar_Change_LanguageApp.swift
+//  Searchbar_Change_Language
+//
+//  Created by Sebastian Klösel on 05.03.22.
+//
+
+import SwiftUI
+
+@main
+struct Searchbar_Change_LanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
