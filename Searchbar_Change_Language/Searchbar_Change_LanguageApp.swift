5a17a3e205e5eda251abde7c2594442e15646a04
Sebastian <s.kloesel@gmx.de>
2022-03-05 17:35:34 +0100

    FIXED

--- Searchbar_Change_Language/Searchbar_Change_LanguageApp.swift
+++ Searchbar_Change_Language/Searchbar_Change_LanguageApp.swift
@@ -4,9 +4,14 @@ import SwiftUI
 
 @main
 struct Searchbar_Change_LanguageApp: App {
+    
+    @AppStorage("Language") var languageChange: String = ""
+    
+    
     var body: some Scene {
         WindowGroup {
             TabViewMaster()
+                .environment(\.locale, .init(identifier: languageChange))
         }
     }
 }
